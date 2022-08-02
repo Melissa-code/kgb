d0db374805b6a0f30c642110adefcc991644b253
Melissa Benard <benardmeli@yahoo.fr>
2022-08-02 18:24:15 +0200

    template

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-hello world
+<?php
+$page_description = "Site du KGB qui liste les missions secrètes. En tant qu'administrateur, vous pourvez créer, modifier ou supprimer les missions."; 
+$page_title = "Présentation du site du KGB"; 
+$page_content = "<h1>Bienvenu sur le site du KGB</h1>";
+require_once('./views/common/template.php');
