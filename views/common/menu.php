4e73dc74abbde85750f09472060d00b0494311fc
Melissa Benard <benardmeli@yahoo.fr>
2022-08-02 19:05:05 +0200

    menu and footer

---
+++ views/common/menu.php
@@ -0,0 +1,34 @@
+<!--------------- Navigation ----------------->
+
+<nav id="menu" class="navbar navbar-expand-lg navbar-light px-5">
+
+    <div class="container-fluid">
+        <!-- Logo -->
+        <div class="logo mx-4">
+            <a class="navbar-brand" href="#">
+                <img src="./public/assets/images/logo_kgb.png" style="height: 100px"/>
+            </a>
+        </div>
+
+        <!-- Button for burger menu-->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <!-- Target of the button -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mx-4 mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Missions</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Connexion</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</nav>
